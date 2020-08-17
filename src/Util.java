791e8b372673f5c0ec58b1602607a05c2c3f3a83
Henrique Andrade <henrique.andrade22@hotmail.com>
2020-08-17 18:19:05 -0300

    Fix getFileContent function

--- src/Util.java
+++ src/Util.java
@@ -1,17 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Util {
     public static String getFileContent(String path) {
+        String content = Constant.EMPTY_STRING;
+
         try {
             Scanner scanner = new Scanner(new File(path));
 
             while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                
+                content += scanner.nextLine() + System.lineSeparator();
             }
 
             scanner.close();
@@ -19,19 +18,6 @@ public class Util {
             e.printStackTrace();
         }
 
-        return "";
-    }
-
-    public static void writeFile(String pathFile, String content) {
-        try {
-            FileWriter outputFile = new FileWriter(pathFile);
-
-            outputFile.write(content);
-            outputFile.close();
-
-            System.out.println(String.format("\"%s\" file created.", pathFile));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return content;
     }
 }
