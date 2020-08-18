823545c5de76e94abef9186347366cad88794603
Henrique Andrade <henrique.andrade22@hotmail.com>
2020-08-17 23:39:48 -0300

    Update constant file

--- src/Constant.java
+++ src/Constant.java
@@ -1,7 +1,15 @@
 public class Constant {
     public static final String INPUT_FILE_PATH = "./assets/input.txt";
 
-    public static final String OUTPUT_FILE_PATH = "./assets/output.txt";
-
     public static final String EMPTY_STRING = "";
+
+    public static final char CARRIAGE_RETURN_CHAR = '\r';
+
+    public static final char LINE_FEED_CHAR = '\n';
+
+    public static final int JUMP_VALUE = 1;
+
+    public static final int LOWEST_ASCII_INDEX = 0;
+
+    public static final int HIGHEST_ASCII_INDEX = 127;
 }
